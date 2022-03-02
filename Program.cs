9c4d5065f84b296e1b6679f0f31df7c25b55d0f7
medusa <gerryfrank720@gmail.com>
2022-03-03 00:05:41 +0300

    Arrays

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ namespace ConsoleApp1
                 Console.WriteLine("The value of i is : {0} ", i);
             }
 
+            //Arays
+            int[] p = new int[] { 1, 2, 3, 4 };
+            Console.WriteLine("p for : {0}", p[1]); // we expect 2
+
             //ForEach loop
             char[] myArray = { 'H', 'e', 'l', 'l', 'o' };
             foreach(char ch in myArray)
